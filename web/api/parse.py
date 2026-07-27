ecd48d9ea4f54b091042923b40509398c6c34f9d
Chishti <catterpi1432@gmail.com>
2026-07-27 14:39:50 +0530

    Update parse.py

--- web/api/parse.py
+++ web/api/parse.py
@@ -1,6 +1,6 @@
 """ serverless function, parse VIT FFCS registration PDFs and merge
 faculty's ratings. Exposes a Flask `application`
-this file is deployable as-is /api,parse.
+this file is deployable as-is /api,parse
 """
 from __future__ import annotations
 
